b9f1509d4b9ddd3d78860265110a1201ede536ac
Andrew Kyu <andrewkyu95@gmail.com>
2018-12-29 15:56:54 -0800

    Added register API route with validators to check if conditions are met for each input fields

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,76 @@
+//Importing Express
 const express = require('express');
 const router = express.Router();
 
-//@route          GET api/users/test
-//@description    Tests user routes
-//@access         Public
-router.get('/test', (req, res)=> res.json({msg: "Users works"}));
+//Encryptions
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+
+//Validations
+const validateRegisterInput = require('../../validation/register');
+
+//MongoDB Schemas
+const User = require('../../models/User');
+
+
+/*
+------------------------------------------------|
+|    @route         GET api//users/test         |
+|    @description   Test user route             | 
+|    @access        Public                      |
+------------------------------------------------|
+*/
+// router.get('/test', (req, res)=> res.json({msg: "Users works"}));
+
+router.get('/test', (req, res)=> {
+    console.log(req.body);
+    res.send({status: 'success'});
+});
+
+/*
+------------------------------------------------|
+|    @route         POST api//users/registration|
+|    @description   Registering a user          | 
+|    @access        Public                      |
+------------------------------------------------|
+*/
+
+router.post('/register', (req, res) => {
+
+    //Checks validations from ../../validation/register
+    console.log(`testing.. ${req}`);
+    const {errors, isValid} = validateRegisterInput(req.body);
+
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({email: req.body.email}).then(user => {
+        if(user)
+        {
+            return res.status(400).json({ email: "Email already exists" });
+        }else
+        {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                  if (err) throw err;
+                  newUser.password = hash;
+                  newUser
+                    .save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+                });
+              });
+        } 
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
+//const profile = require('./routes/api/profile');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require('./config/keys').mongoURI;
@@ -15,14 +20,16 @@ mongoose
   .then(()=>console.log('MongoDB connected'))
   .catch(err=> console.log(err));
 
-  //use routes
-  app.use('/api/users', users);
-  app.use('/api/profile', profile);
+//password middleware
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
+
+//use routes
+app.use('/api/users', users);
+// app.use('/api/profile', profile);
 
 const port = process.env.PORT || 5000;
-
-app.get('/', (req, res)=>{
-  res.send('hello world');
-});
 
 app.listen(port, ()=> console.log(`server running on port ${port}`));
