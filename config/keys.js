20226a95afc1f3c2827f60afe4ec558e08a23787
Andrew Kyu <andrewkyu95@gmail.com>
2019-04-17 21:22:23 -0700

    cleaned up a bit of code

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,3 @@
-// module.exports = {
-//   mongoURI: 'mongodb://andrew:andrew1@ds239968.mlab.com:39968/mentorcrowd',
-//   secretOrKey: "secret"
-// }
 if(process.env.NODE_ENV === 'production'){
   module.exports = require('./keys_prod');
 }else{
