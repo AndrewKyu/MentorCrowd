2c8c1464f36f1d6f3f33ab5f083152918455d565
Andrew Kyu <andrewkyu95@gmail.com>
2019-04-30 13:44:06 -0700

    renamed credentials

--- config/credential.js
+++ config/credential.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = require('./credentials_prod');
   }else{
-    module.exports = require('./credential_dev');
+    module.exports = require('./credentials_dev');
   }
