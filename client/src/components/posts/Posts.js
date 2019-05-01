6f7889051edcaa3ace318713259111cdfafb5e5c
Andrew Kyu <andrewkyu95@gmail.com>
2019-05-01 01:56:02 -0700

    error handling for no profile users. links and newsfeed are disabled

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,28 +4,39 @@ import { connect } from 'react-redux';
 import PostForm from './PostForm';
 import Spinner from '../common/Spinner';
 import { getPosts } from '../../actions/postActions';
+import { getCurrentProfile } from '../../actions/profileActions';
 import PostFeed from './PostFeed';
+import { Link } from 'react-router-dom';
 
 class Posts extends Component {
   componentDidMount(){
     this.props.getPosts();
+    this.props.getCurrentProfile();
   }
   
   render() {
     const { posts, loading } = this.props.post;
+    const { profile } = this.props.profile;
     let postContent;
-
+    // console.log(posts);
     if(posts === null || loading) {
       postContent = <Spinner />
     }else{
-      postContent = <PostFeed posts={posts} />
+      postContent = (profile === null || Object.keys(profile).length === 0) ? (
+        <div className="mt-4">
+          <p className="text-center">You don't have a profile yet. <Link to="/dashboard">Please create one here</Link> to get connected with mentors</p>
+          {/* <PostFeed posts={posts} /> */}
+        </div>
+      ) : <PostFeed posts={posts} />;
+      // postContent = <PostFeed posts={posts} />
     }
     return (
       <div className="feed">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <PostForm />
+              {/* <PostForm /> */}
+              {(profile === null || Object.keys(profile).length === 0) ? null : <PostForm />}
               {postContent}
             </div>
           </div>
@@ -38,12 +49,16 @@ class Posts extends Component {
 Posts.propTypes = {
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
-  getPosts: PropTypes.func.isRequired
+  getPosts: PropTypes.func.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
   post: state.post,
-  auth: state.auth
+  auth: state.auth,
+  profile: state.profile
 });
 
-export default connect(mapStateToProps, { getPosts })(Posts);
+export default connect(mapStateToProps, { getPosts, getCurrentProfile })(Posts);
+
